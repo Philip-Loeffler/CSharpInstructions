5f8d598957b3b404cdc21146675eed2b2b8024ca
philip loeffler <philiploeffler@gmail.com>
2020-12-02 20:26:25 -0500

    prompt

---
+++ ch5_OptionalArguments/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ch5_OptionalArguments {
+    class Program {
+        static void Main(string[] args) {
+        Console.WriteLine("optional args");
+        int nb1 = 22;
+        int nb2 = 44;
+        int nb3 = 11;
+
+
+        Console.WriteLine("sum is" + Sum(nb1, nb2, nb3));
+
+        Console.WriteLine("bye!");
+
+            }
+
+        // n3 is an optional param
+        static int Sum(int n1, int n2, int n3 = 0) {
+        return n1 + n2 + n3;
+            }
+        }
+    }
--- ch5_methods/Program.cs
+++ ch5_methods/Program.cs
@@ -5,12 +5,33 @@ namespace ch5_methods {
         static void Main(string[] args) {
         Console.WriteLine("Calls some methods!");
 
-        Console.WriteLine("Enter a whole number!");
-        int n = int.Parse(Console.ReadLine());
-        Console.WriteLine("you entered the number!" + n);
+        int n = GetInt("Enter a whole number");
+        Console.WriteLine("you entered the number" + n);
+
+        int age = GetInt("Enter actor age: ");
+        Console.WriteLine("actors age" + age);
 
 
 
+        Console.WriteLine("bye");
+
+
+            }
+
+        private static int GetInt(String prompt) {
+        int n = 0;
+        Boolean success = false;
+        while (!success) {
+        Console.Write(prompt);
+
+        try {
+        n = int.Parse(Console.ReadLine());
+        success = true;
+            } catch (Exception ex) {
+        Console.WriteLine("invalid entry. not a whole number. try again");
+            }
+            }
+        return n;
             }
         }
     }
