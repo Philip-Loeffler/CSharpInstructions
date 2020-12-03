b0f65318b1983abfab7411806f9daac5b62d0137
philip loeffler <philiploeffler@gmail.com>
2020-12-02 19:10:33 -0500

    adjusted ignore file

---
+++ ch5_methods/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ch5_methods {
+    class Program {
+        static void Main(string[] args) {
+        Console.WriteLine("Calls some methods!");
+
+        Console.WriteLine("Enter a whole number!");
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine("you entered the number!" + n);
+
+
+
+            }
+        }
+    }
---
+++ tableOfPowers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace tableOfPowers {
+    class Program {
+        static void Main(string[] args) {
+        Console.WriteLine("Welcome to the Squares and cubes table");
+        int squared;
+        int cubed;
+        Console.WriteLine("enter an integer");
+        int number = Int32.Parse(Console.ReadLine());
+
+        for (int i = 0; i < number; i++) {
+        squared = i * i;
+        cubed = i * i * i;
+        Console.Write(i);
+        Console.Write("\t" + squared);
+        Console.Write("\t" + cubed);
+            }
+            }
+        }
+    }
